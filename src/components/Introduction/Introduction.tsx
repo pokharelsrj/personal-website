8f1368a8a377a8d2d85c657ceca76ed58087edf3
pokharelsrj <pokharelsrj@gmail.com>
2021-06-30 15:43:50 +0545

    Add image and edit about me introduction

--- src/components/Introduction/Introduction.tsx
+++ src/components/Introduction/Introduction.tsx
@@ -1,33 +1,94 @@
 import React from "react";
-import { CssBaseline, Typography, Container } from "@material-ui/core";
+import {
+  CssBaseline,
+  Typography,
+  Container,
+  Grid,
+  Button,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import img1 from "../../assets/img1.jpeg";
 
+const useStyles = makeStyles((theme) => ({
+  intro: {
+    marginTop: theme.spacing(6),
+    marginBottom: theme.spacing(7),
+  },
+  aboutMe: {},
+  aboutMeDiv: {},
+  img1: {
+    borderRadius: "10px",
+  },
+}));
 export const Introduction: React.FC = () => {
+  const classes = useStyles();
   return (
     <>
       <CssBaseline />
-      <Container maxWidth="md">
-        <Typography variant="h1" align="center" gutterBottom>
-          Hello
-        </Typography>
-        <Typography variant="h1" align="center" gutterBottom>
-          I&apos;m Srijan
-        </Typography>
+      <Container maxWidth="sm" className={classes.intro}>
         <Typography
-          variant="h4"
+          component="h1"
+          variant="h3"
           align="center"
-          color="textSecondary"
+          color="textPrimary"
           gutterBottom
-          paragraph
         >
-          I am pursuing my Bachelor's Degree in Computer Science from Chitkara
-          University. Apart from my love for Computers, I'm fascinated by
-          Physics and Scientific Explorations. Sports are an inseparable part of
-          my life. I have a unique affinity for Film Making and Photography.
-        </Typography>
-        <Typography variant="h4" align="center" color="textSecondary">
-          You&apos;ll dive deep into my life through this website
+          Hi, I&apos;m Srijan
         </Typography>
       </Container>
+      <div className={classes.aboutMeDiv}>
+        <Container maxWidth="md" className={classes.aboutMe}>
+          <Grid container spacing={5} alignItems="center" justify="center">
+            <Grid item lg={7}>
+              <Grid
+                container
+                justify="center"
+                alignItems="center"
+                spacing={8}
+                direction="column"
+              >
+                <Grid item lg={12}>
+                  <Typography
+                    variant="h5"
+                    align="center"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    I am fascinated by Science and love Sports. I also have a
+                    unique affinity for Film making and Photography.
+                  </Typography>
+                </Grid>
+                <Grid item lg={12}>
+                  <Typography
+                    variant="h5"
+                    align="center"
+                    color="textSecondary"
+                    component="p"
+                    gutterBottom
+                  >
+                    I'm pursuing Computer Science Engineering from Chitkara
+                    University in India.
+                  </Typography>
+                </Grid>
+                <Grid item lg={12}>
+                  <Button variant="outlined" color="primary">
+                    Learn More About Me
+                  </Button>
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid item lg={5}>
+              <img
+                src={img1}
+                alt="img1"
+                width="100%"
+                height="auto"
+                className={classes.img1}
+              />
+            </Grid>
+          </Grid>
+        </Container>
+      </div>
     </>
   );
 };
