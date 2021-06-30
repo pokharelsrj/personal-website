ead0cf5f9e1afb63362b77112fc223441abd616b
pokharelsrj <pokharelsrj@gmail.com>
2021-06-30 15:42:40 +0545

    Remove global theme for colors

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,6 @@ import { MuiThemeProvider, createMuiTheme } from "@material-ui/core";
 import { Shadows } from "@material-ui/core/styles/shadows";
 
 const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: "#FEF6EB",
-    },
-    secondary: {
-      main: "#C25B56",
-    },
-  },
   shadows: Array(25).fill("none") as Shadows,
 });
 
