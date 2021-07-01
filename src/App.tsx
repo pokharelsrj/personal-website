3eeebdf4e5254111545cff0f64fdee567f5f0d3f
pokharelsrj <pokharelsrj@gmail.com>
2021-07-01 20:58:39 +0545

    Hide overflown content

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
 import Home from "./pages/Home";
 import { MuiThemeProvider, createMuiTheme } from "@material-ui/core";
 import { Shadows } from "@material-ui/core/styles/shadows";
+import { makeStyles } from "@material-ui/core/styles";
 
 const theme = createMuiTheme({
   shadows: Array(25).fill("none") as Shadows,
 });
 
-export const App: React.FC = () => (
-  <MuiThemeProvider theme={theme}>
-    <Home />
-  </MuiThemeProvider>
-);
+const useStyles = makeStyles((theme) => ({
+  mainDiv: {
+    overflowX: "hidden",
+  },
+}));
+
+export const App: React.FC = () => {
+  const classes = useStyles();
+
+  return (
+    <MuiThemeProvider theme={theme}>
+      <div className={classes.mainDiv}>
+        <Home />
+      </div>
+    </MuiThemeProvider>
+  );
+};
 
 export default App;
