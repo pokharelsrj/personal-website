8faa6f2235a56b5a220b5d4a7b4876df08da2ec9
pokharelsrj <pokharelsrj@gmail.com>
2021-07-07 21:02:19 +0545

    Remove webpack and use CRA build

--- index.js
+++ index.js
@@ -30,9 +30,9 @@ mongoose.set("useCreateIndex", true);
 
 if (process.env.NODE_ENV === "production") {
   //Set static folder
-  app.use(express.static("client/dist"));
+  app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
