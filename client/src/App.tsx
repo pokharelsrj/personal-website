4860736ac979eaf7112e505cef6d3a7f87be30f1
pokharelsrj <pokharelsrj@gmail.com>
2021-07-09 12:51:54 +0545

    Remove deprecated createMuiTheme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,10 @@
-import { MuiThemeProvider, createMuiTheme } from "@material-ui/core";
+import { MuiThemeProvider, createTheme } from "@material-ui/core";
 import { Shadows } from "@material-ui/core/styles/shadows";
 import { makeStyles } from "@material-ui/core/styles";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Routes } from "./routes/Routes";
 
-const theme = createMuiTheme({
+const theme = createTheme({
   shadows: Array(25).fill("none") as Shadows,
 });
 
