4469715c6a2b34adbf179e353ea7592bddfbc668
pokharelsrj <pokharelsrj@gmail.com>
2021-07-12 13:59:59 +0545

    Add BlogsBody to blogs

--- client/src/pages/Blogs/Blogs.tsx
+++ client/src/pages/Blogs/Blogs.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../../components/Header";
 import { CssBaseline } from "@material-ui/core";
+import BlogsBody from "../../components/BlogsBody";
 
 export const Blogs: React.FC = () => {
   return (
     <>
       <CssBaseline />
       <Header />
+      <BlogsBody />
     </>
   );
 };
