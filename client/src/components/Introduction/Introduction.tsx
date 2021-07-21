3fad8da5e41304683e4959b367aaa97b13a216f5
pokharelsrj <pokharelsrj@gmail.com>
2021-07-21 14:22:20 +0545

    Add spacing after Introduction compoent

--- client/src/components/Introduction/Introduction.tsx
+++ client/src/components/Introduction/Introduction.tsx
@@ -13,79 +13,84 @@ const useStyles = makeStyles((theme) => ({
   img1: {
     borderRadius: "10px",
   },
+  container: {
+    marginBottom: theme.spacing(8),
+  },
 }));
 export const Introduction: React.FC = () => {
   const classes = useStyles();
   return (
     <>
-      <Container maxWidth="sm" className={classes.intro}>
-        <Typography
-          component="h1"
-          variant="h3"
-          align="center"
-          color="textPrimary"
-          gutterBottom
-        >
-          Hi, I&apos;m Srijan
-        </Typography>
-      </Container>
-      <div className={classes.aboutMeDiv}>
-        <Container maxWidth="md" className={classes.aboutMe}>
-          <Grid
-            container
-            spacing={5}
-            alignItems="center"
-            justifyContent="center"
+      <div className={classes.container}>
+        <Container maxWidth="sm" className={classes.intro}>
+          <Typography
+            component="h1"
+            variant="h3"
+            align="center"
+            color="textPrimary"
+            gutterBottom
           >
-            <Grid item lg={7}>
-              <Grid
-                container
-                justifyContent="center"
-                alignItems="center"
-                spacing={8}
-                direction="column"
-              >
-                <Grid item lg={12}>
-                  <Typography
-                    variant="h5"
-                    align="center"
-                    color="textSecondary"
-                    component="p"
-                  >
-                    I am fascinated by Science and love Sports. I also have a
-                    unique affinity for Film making and Photography.
-                  </Typography>
-                </Grid>
-                <Grid item lg={12}>
-                  <Typography
-                    variant="h5"
-                    align="center"
-                    color="textSecondary"
-                    component="p"
-                    gutterBottom
-                  >
-                    I'm pursuing Computer Science Engineering from Chitkara
-                    University in India.
-                  </Typography>
-                </Grid>
-                <Grid item lg={12}>
-                  <Button variant="outlined" color="primary">
-                    Learn More About Me
-                  </Button>
+            Hi, I&apos;m Srijan
+          </Typography>
+        </Container>
+        <div className={classes.aboutMeDiv}>
+          <Container maxWidth="md" className={classes.aboutMe}>
+            <Grid
+              container
+              spacing={5}
+              alignItems="center"
+              justifyContent="center"
+            >
+              <Grid item lg={7}>
+                <Grid
+                  container
+                  justifyContent="center"
+                  alignItems="center"
+                  spacing={8}
+                  direction="column"
+                >
+                  <Grid item lg={12}>
+                    <Typography
+                      variant="h5"
+                      align="center"
+                      color="textSecondary"
+                      component="p"
+                    >
+                      I am fascinated by Science and love Sports. I also have a
+                      unique affinity for Film making and Photography.
+                    </Typography>
+                  </Grid>
+                  <Grid item lg={12}>
+                    <Typography
+                      variant="h5"
+                      align="center"
+                      color="textSecondary"
+                      component="p"
+                      gutterBottom
+                    >
+                      I'm pursuing Computer Science Engineering from Chitkara
+                      University in India.
+                    </Typography>
+                  </Grid>
+                  <Grid item lg={12}>
+                    <Button variant="outlined" color="primary">
+                      Learn More About Me
+                    </Button>
+                  </Grid>
                 </Grid>
               </Grid>
+              <Grid item lg={5}>
+                <img
+                  src={img1}
+                  alt="img1"
+                  width="100%"
+                  height="auto"
+                  className={classes.img1}
+                />
+              </Grid>
             </Grid>
-            <Grid item lg={5}>
-              <img
-                src={img1}
-                alt="img1"
-                width="100%"
-                height="auto"
-                className={classes.img1}
-              />
-            </Grid>
-          </Grid>
-        </Container>
+          </Container>
+        </div>
       </div>
     </>
   );
