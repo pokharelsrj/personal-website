782033470626f45f239e8be025175360cd9c5907
pokharelsrj <pokharelsrj@gmail.com>
2021-07-21 14:21:49 +0545

    Add Skiils component to Home page

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -3,6 +3,7 @@ import Header from "../../components/Header";
 import Introduction from "../../components/Introduction";
 import Footer from "../../components/Footer";
 import { CssBaseline } from "@material-ui/core";
+import Skills from "../../components/Skills";
 
 export const Home: React.FC = () => {
   return (
@@ -10,6 +11,7 @@ export const Home: React.FC = () => {
       <CssBaseline />
       <Header />
       <Introduction />
+      <Skills />
       <Footer />
     </>
   );
