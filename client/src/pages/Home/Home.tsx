f77673ffe82db7dc90587222ff22d619a02b6910
pokharelsrj <pokharelsrj@gmail.com>
2021-07-23 21:01:26 +0545

    Add Hobbies to Home Page

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -4,6 +4,7 @@ import Introduction from "../../components/Introduction";
 import Footer from "../../components/Footer";
 import { CssBaseline } from "@material-ui/core";
 import Skills from "../../components/Skills";
+import Hobbies from "../../components/Hobbies";
 
 export const Home: React.FC = () => {
   return (
@@ -12,6 +13,7 @@ export const Home: React.FC = () => {
       <Header />
       <Introduction />
       <Skills />
+      <Hobbies />
       <Footer />
     </>
   );
