2a8046aa7503ce07d9849ffe1da2fe81f436831b
pokharelsrj <pokharelsrj@gmail.com>
2021-07-29 22:05:04 +0545

    Add route for NewBlog component

--- client/src/routes/Routes.tsx
+++ client/src/routes/Routes.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Blogs from "../pages/Blogs";
 import Home from "../pages/Home";
+import NewBlog from "../pages/NewBlog";
 
 export const Routes: React.FC = () => (
   <Switch>
     <Route exact path="/" component={Home} />
     <Route exact path="/blogs" component={Blogs} />
+    <Route exact path="/blogs/new" component={NewBlog} />
   </Switch>
 );
