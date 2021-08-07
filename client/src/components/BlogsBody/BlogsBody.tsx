86aadfb372ada4eb7fcdc7b7568f2887611f8a51
pokharelsrj <pokharelsrj@gmail.com>
2021-08-07 10:34:13 +0545

    Add dummy blogs to BlogsBody component

--- client/src/components/BlogsBody/BlogsBody.tsx
+++ client/src/components/BlogsBody/BlogsBody.tsx
@@ -1,11 +1,15 @@
 import React from "react";
-import { Grid, Typography, Container } from "@material-ui/core";
+import { Grid, Container } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import Blog from "../BlogCard";
 
 const useStyles = makeStyles((theme) => ({
   mainDiv: {
     marginTop: theme.spacing(6),
     marginBottom: theme.spacing(7),
+  },
+  illustrations: {
+    height: "900px",
   },
 }));
 export const BlogsBody: React.FC = () => {
@@ -13,9 +17,15 @@ export const BlogsBody: React.FC = () => {
   return (
     <>
       <Container maxWidth="md" className={classes.mainDiv}>
-        <Grid container>
+        <Grid container spacing={8} justifyContent="center" alignItems="center">
           <Grid item>
-            <Typography>What's New?</Typography>
+            <Blog fileName="blog1.md" date={new Date()} />
+          </Grid>
+          <Grid item>
+            <Blog fileName="blog2.md" date={new Date()} />
+          </Grid>
+          <Grid item>
+            <Blog fileName="blog3.md" date={new Date()} />
           </Grid>
         </Grid>
       </Container>
