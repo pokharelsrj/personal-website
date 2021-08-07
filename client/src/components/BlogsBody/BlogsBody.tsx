0ca231dc6dfa806a3148d1a70a380cc019510f35
pokharelsrj <pokharelsrj@gmail.com>
2021-08-07 10:36:48 +0545

    Edit Blog to BlogCard in BlogsBody component

--- client/src/components/BlogsBody/BlogsBody.tsx
+++ client/src/components/BlogsBody/BlogsBody.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Grid, Container } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import Blog from "../BlogCard";
+import BlogCard from "../BlogCard";
 
 const useStyles = makeStyles((theme) => ({
   mainDiv: {
@@ -19,13 +19,13 @@ export const BlogsBody: React.FC = () => {
       <Container maxWidth="md" className={classes.mainDiv}>
         <Grid container spacing={8} justifyContent="center" alignItems="center">
           <Grid item>
-            <Blog fileName="blog1.md" date={new Date()} />
+            <BlogCard fileName="blog1.md" date={new Date()} />
           </Grid>
           <Grid item>
-            <Blog fileName="blog2.md" date={new Date()} />
+            <BlogCard fileName="blog2.md" date={new Date()} />
           </Grid>
           <Grid item>
-            <Blog fileName="blog3.md" date={new Date()} />
+            <BlogCard fileName="blog3.md" date={new Date()} />
           </Grid>
         </Grid>
       </Container>
