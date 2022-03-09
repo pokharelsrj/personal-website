bf393c876cb42ad46ec120fd566d3c193765c1aa
Srijan Pokharel <srijan.pokharel@medly.com>
2022-03-09 19:51:22 +0530

    remove unused var

--- client/src/components/Blog/Blog.tsx
+++ client/src/components/Blog/Blog.tsx
@@ -1,4 +1,7 @@
-import React from "react";
+import { Container, Grid } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Markdown from "../Markdown";
 // import { makeStyles } from "@material-ui/core/styles";
 
 // const useStyles = makeStyles({
@@ -8,9 +11,32 @@ import React from "react";
 // });
 
 export const Blog: React.FC = () => {
+  const { id } = useParams();
+  const [content, setContent] = useState("");
+
+  useEffect(() => {
+    import(`../../assets/blogs/${id}`)
+      .then((res) => {
+        fetch(res.default)
+          .then((res) => res.text())
+          .then((res) => setContent(res))
+          .catch((err) => console.log(err));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [id]);
   // const classes = useStyles();
 
-  return <>Hello</>;
+  return (
+    <>
+      <Container maxWidth="md">
+        <Grid item>
+          <Markdown>{content}</Markdown>
+        </Grid>
+      </Container>
+    </>
+  );
 };
 
 Blog.displayName = "Blog";
